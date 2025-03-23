a0aa5174e94fcb3c69f193f5a085098fdd16416d
Robby Kloss <robk113355@gmail.com>
2025-03-23 15:15:35 -0500

    Created 2 helpers and the Char function

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
-
 #include <iostream>
 using namespace std;
 
 //required function prototypes
+int arraySizeFinder(const char numString[]);
+int periodIndexFinder(const char numString[]);
+
 bool characteristic(const char numString[], int& c);
 bool mantissa(const char numString[], int& numerator, int& denominator);
 
 bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
-bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len); 
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len);
@@ -16,21 +18,23 @@ int main()
 {
     //this c-string, or array of 8 characters, ends with the null terminating character '\0'
     //['1', '2', '3', '.', '4', '5', '6', '\0']
-    const char number[] = "123.456"; 
+    const char number[] = "123.456";
     int c, n, d;
-
+    //cout << arraySizeFinder(number) << endl;
+    //cout << periodIndexFinder(number) << endl;
+   
     //if both conversions from c-string to integers can take place
-    if(characteristic(number, c) && mantissa(number, n, d))
+    if (characteristic(number, c) && mantissa(number, n, d))
     {
         //do some math with c, n, and d
-        cout<<"c: "<<c<<endl;
-        cout<<"n: "<<n<<endl;
-        cout<<"d: "<<d<<endl;
+        cout << "c: " << c << endl;
+        cout << "n: " << n << endl;
+        cout << "d: " << d << endl;
     }
     else //at least one of the conversions failed
     {
         //handle the error on input
-        cout<<"Error on input"<<endl;
+        cout << "Error on input" << endl;
     }
 
     //room for 9 characters plus the null terminating character
@@ -45,38 +49,77 @@ int main()
 
     c2 = 2;
     n2 = 2;
-    d2 = 3; 
+    d2 = 3;
 
     //if the c-string can hold at least the characteristic
-    if(add(c1, n1, d1, c2, n2, d2, answer, 10))
+    if (add(c1, n1, d1, c2, n2, d2, answer, 10))
     {
         //display string with answer 4.1666666 (cout stops printing at the null terminating character)
-        cout<<"Answer: "<<answer<<endl;
+        cout << "Answer: " << answer << endl;
     }
     else
     {
         //display error message
-        cout<<"Error on add"<<endl;
+        cout << "Error on add" << endl;
     }
 
-    if(divide(c1, n1, d1, c2, n2, d2, answer, 10))
+    if (divide(c1, n1, d1, c2, n2, d2, answer, 10))
     {
         //display string with answer
-        cout<<"Answer: "<<answer<<endl;
+        cout << "Answer: " << answer << endl;
     }
     else
     {
         //display error message
-        cout<<"Error on divide"<<endl;
+        cout << "Error on divide" << endl;
     }
 
     return 0;
-} 
+}
 //--
+
+int periodIndexFinder(const char numString[]) {
+    int i = 0;
+    bool hasBeenFound = false;
+    while (hasBeenFound == false)
+    {
+        if (numString[i] == '.')
+        {
+            hasBeenFound = true;
+            return i;
+        }
+        i++;
+    }
+    return 0;
+}
+int arraySizeFinder(const char numString[]) {
+    int i = 0;
+    bool hasBeenFound = false;
+    while (hasBeenFound == false)
+    {
+        if (numString[i] == '\0')
+        {
+            hasBeenFound = true;
+            return i + 1;
+        }
+        i++;
+    }
+    return 0;
+
+};
+
+
 bool characteristic(const char numString[], int& c)
 {
+    int base = 1;
+    c = 0;
+    int periodIndex = periodIndexFinder(numString);
+    for (int i = periodIndex - 1; i >= 0; i--)
+    {
+        c += (numString[i] - 48) * base;
+        base = base * 10;
+    }
     //hard coded return value to make the main() work
-    c = 123;
     return true;
 }
 //--
@@ -131,6 +174,6 @@ bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int l
     result[4] = '2';
     result[5] = '5';
     result[6] = '\0';
-    
+
     return true;
 }
