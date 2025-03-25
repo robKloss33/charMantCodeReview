6dfc873c9ba03553089d06d37a234588e4ebd2d3
daniel-plutchak <dplutchak@carthage.edu>
2025-03-25 10:00:08 -0500

    Created math.cpp

---
+++ math.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+
+bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
+    
+}
+
+bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
+
+}
+
+bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
+
+}
+bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len){
+
+}
